25573ae0c5fdf8bc07131b7a5685fd3314c075bd
cesantosv <cesantosv@unal.edu.co>
2018-08-31 15:31:00 -0500

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+  //  MongooseModule.forRoot('mongodb://localhost:27017/nest'),
     TodosModule,
     UsersModule],
    controllers: [],
