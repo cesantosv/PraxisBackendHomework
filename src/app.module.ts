f390fb751c0ace937add1ad4bc7080c4d2ab5901
MrPauL91 <jupvillegasgo@unal.edu.co>
2018-09-03 16:59:33 -0500

    CORRECT PORT AND MONGODB CONNECTION WITH HEROKU

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    MongooseModule.forRoot('mongodb://admin:admin123@ds237832.mlab.com:37832/nest'),
     TodosModule,
     UsersModule],
    controllers: [],
