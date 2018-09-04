81a36b1dd1a6fe53b6d8c4938bebc0a28fe7fe7d
cesantosv <cesantosv@unal.edu.co>
2018-09-03 20:21:51 -0500

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   const options = new DocumentBuilder()
     .setTitle('Todos example')
     .setDescription('The Todos API description')
